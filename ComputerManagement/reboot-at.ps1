813b0fbf282a8b503e1958ea98e57a42b98f76f3
Lane Duncan <laned@smu.edu>
2022-02-16 08:02:09 -0600

    Added -Linux check and remote pssession

--- ComputerManagement/reboot-at.ps1
+++ ComputerManagement/reboot-at.ps1
@@ -3,10 +3,13 @@
     Reboots a computer at a specified date and time
 .DESCRIPTION
     uses the Windows shutdown.exe application with the /t timer switch to reboot a computer at a specified date and time.
+    uses the Linux shutdown application with the +<min> timer switch to reboot a computer at a specified time
 .PARAMETER computerName
     (optional) If specified, reboots the remote computer <computerName>
 .PARAMETER RestartTime
     (mandatory) The time (required) and date (optional) to reboot the computer.  If the time only is specified, the date is assumed to be the current date.
+.PARAMETER Linux
+    (optional) If "-Linux" is specified at the command line, use remote pssession to reboot a remote Linux/MacOS computer
 .EXAMPLE
     ./reboot-at -RestartTime "8PM"
         restarts the local host at 8:00pm today
@@ -15,9 +18,13 @@
         restarts the host "mysrv.smu.edu" at 7:43pm on 2/14/2022
 .NOTES
     FunctionName : reboot-at.ps1
-    Version: 1.00.00
+    Version: 1.05.00
     Created by   : Lane Duncan
     Date Coded   : 2/14/2022
+
+    CHANGELOG
+        v1.05.00    20220216    Lane Duncan
+            Added Linux check and remote pssession
 .LINK
     https://wiki.smu.edu/display/ITS/Powershell+Cmdlets+and+Scripts
 #>
@@ -25,14 +32,25 @@ param(
     [parameter()]
     [string]$computerName,
     [parameter(mandatory)]
-    [datetime]$RestartTime
-)
+    [datetime]$RestartTime,
+    [switch]$Linux
+    )
 [datetime]$CurrentTime = Get-Date
 [int]$WaitSeconds = ( $RestartTime - $CurrentTime ).TotalSeconds
-
-$rebootParams = @("/t $($WaitSeconds)", "/r"
-)
-if ($PSBoundParameters.ContainsKey("computerName")) {
-    $rebootParams.Add("/m \\$($computerName)")
+if ($Linux) # Linux/MacOS 
+{
+    if (-not($PSBoundParameters.ContainsKey("comoputerName"))){
+        $computerName="."
+    }
+    $rebootParams = @("-r +$([int]$($waitseconds /60))")
+    $session = New-PSSession -Computername $($computerName)
+    Invoke-Command -ScriptBlock {"shutdown"} -ArgumentList $rebootParams -Session $session
 }
-Start-Process shutdown -ArgumentList $rebootParams
+else # Windows
+{
+    $rebootParams = @("/t $($WaitSeconds)", "/r")
+    if ($PSBoundParameters.ContainsKey("computerName")) {
+        $rebootParams.Add("/m \\$($computerName)")
+    }
+    Start-Process shutdown -ArgumentList $rebootParams
+}
